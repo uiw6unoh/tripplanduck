5ca1203d97b211c310d529547a36f030434a977c
박노경 <gerie99@naver.com>
2022-10-03 02:50:57 +0900

    no message

--- TripPlanDuck/src/main/java/com/tripplan/duck/withduck/controller/WithDuckController.java
+++ TripPlanDuck/src/main/java/com/tripplan/duck/withduck/controller/WithDuckController.java
@@ -1,13 +1,18 @@
-package com.tripplan.duck.withduck;
+package com.tripplan.duck.withduck.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.tripplan.duck.withduck.model.service.WithDuckService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequestMapping("/withduck")
 public class WithDuckController {
 	
 	@GetMapping("/withduck/list")
