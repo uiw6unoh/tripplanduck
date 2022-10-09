43c6aece1650b230ecd96bf0169959bb2524a8f6
minjoo <minjoooo127@gmail.com>
2022-10-10 06:10:23 +0900

    ﻿20221010 뷰단 수정/풀 받을래요,,

--- TripPlanDuck/src/main/java/com/tripplan/duck/mypage/controller/MyPageController.java
+++ TripPlanDuck/src/main/java/com/tripplan/duck/mypage/controller/MyPageController.java
@@ -1,22 +1,8 @@
 package com.tripplan.duck.mypage.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.tripplan.duck.member.model.service.MemberService;
-import com.tripplan.duck.member.model.vo.Member;
-import com.tripplan.duck.mypage.model.service.MyPageService;
-import com.tripplan.duck.planner.model.vo.MyPlanner;
-import com.tripplan.duck.trip.model.vo.Comments;
-import com.tripplan.duck.trip.model.vo.Destination;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,31 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/mypage")
 public class MyPageController {
 	
-	@Autowired
-	private MyPageService myPageService;
-	
-	// 마이페이지 메인
-	@GetMapping("")
-	public String Mypage(@RequestParam(required = false, defaultValue = "1") int offset, @RequestParam(defaultValue = "planner") String select, Model model) throws Exception {
+	// 좋아요 한 플래너
+	@GetMapping("/")
+	public String Mypage() throws Exception {
 		
-		// 로그인 데이터로 갈아끼우기
-		Member member = new Member(2, "yeoul", "1234", "김여울", "여리", "yeoul940813@gmail.com", 'M', '0', 'Y', 'F', 20); 
-	
-		Map<String, Object> param = new HashMap<String, Object>();
-		param.put("memberNo", member.getMemberNo());
-		param.put("offset", offset);
-		
-		if(select.equals("planner")) {
-			List<MyPlanner> myPlannerList = myPageService.selectMyPlannerByMNo(param);
-			model.addAttribute("myPlannerList", myPlannerList);
-		}else if(select.equals("trip")) {
-			List<Destination> tripList = myPageService.selectTripByMNo(param);
-			model.addAttribute("tripList", tripList);
-		}else if(select.equals("comment")) {
-			List<Comments> commentsList = myPageService.selectCommentsByMNo(param);
-			model.addAttribute("commentsList", commentsList);		
-		}
-		model.addAttribute("member", member);
 		return "mypage/MypageMain";
 	}
 	
