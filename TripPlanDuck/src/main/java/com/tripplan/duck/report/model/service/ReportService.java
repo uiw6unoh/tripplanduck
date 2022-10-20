19989c8ef0bd77bda0b90b3f67ef432ec66c0b55
uiw6unoh <uiw6unoh@naver.com>
2022-10-20 17:13:34 +0900

    221020 신고 구현 완. 그러나 commentsId 값 제대로 못 불러옴,,,,

--- TripPlanDuck/src/main/java/com/tripplan/duck/report/model/service/ReportService.java
+++ TripPlanDuck/src/main/java/com/tripplan/duck/report/model/service/ReportService.java
@@ -1,14 +1,20 @@
 package com.tripplan.duck.report.model.service;
 
-import java.util.List;
-import java.util.Map;
-
-import com.tripplan.duck.planner.model.vo.Location;
+import com.tripplan.duck.report.model.vo.Report;
 import com.tripplan.duck.trip.model.vo.Comments;
-import com.tripplan.duck.trip.model.vo.Destination;
-import com.tripplan.duck.trip.model.vo.DestinationLike;
-
 
 public interface ReportService {
+
+	int isReported(String reportCategory, String reportNoType);
+
+	Comments getComments(String reportNoType);
+
+	void updateReportCount(Report report);
+
+	void insertCommentReport(Report report);
+
+	Report getReportComments(String reportNoType);
+
+
 
 }
