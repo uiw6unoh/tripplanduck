7cb5078c01ae026dc950a05489ba2a724fc656a7
박노경 <gerie99@naver.com>
2022-10-21 15:57:32 +0900

    채팅 수저어엉

--- TripPlanDuck/src/main/java/com/tripplan/duck/withduck/handler/ChattingHandler.java
+++ TripPlanDuck/src/main/java/com/tripplan/duck/withduck/handler/ChattingHandler.java
@@ -2,8 +2,6 @@ package com.tripplan.duck.withduck.handler;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -20,109 +18,37 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tripplan.duck.withduck.model.vo.WithDuck;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ChattingHandler extends TextWebSocketHandler {
+private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
 
-//	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
-//
-//	@Override
-//	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//		log.info("#ChattingHandler, afterConnectionEstablished");
-//		sessionList.add(session);
-//		log.info(session.getId() + "님이 입장하셨습니다.");
-//		
-//	}
-//	
-//	
-//    @Override
-//	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-//		log.info("#ChattingHandler, handleMessage");
-//		System.out.println("받은 session 정보 : " + session.getAttributes());
-//		System.out.println("입장 샷 : " + message);
-//		for(WebSocketSession s : sessionList) {
-//			if(session.getId() == s.getId()) {
-//				continue;
-//			}
-//			s.sendMessage(new TextMessage(session.getId() + ":" + message.getPayload() + ":" + session.getAttributes()));
-//			System.out.println("sessiong.getId(): " + session);
-//		}
-//		
-//		System.out.println("sessionList : " + sessionList);
-//		
-//	}
-//    
-//    
-//	
-//    @Override
-//	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-//		
-//		log.info("#ChattingHandler, afterConnectionClosed");
-//		sessionList.remove(session);
-//		/*
-//		
-//		log.info(session.getPrincipal().getName() + "님이 퇴장하셨습니다.");
-//		*/
-//	}
-
-	// (<"bang_id", 방ID>, <"session", 세션>) - (<"bang_id", 방ID>, <"session", 세션>) - (<"bang_id", 방ID>, <"session", 세션>) 형태 
-	private List<Map<String, Object>> sessionList = new ArrayList<Map<String, Object>>();
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		log.info("#ChattingHandler, afterConnectionEstablished");
+		sessionList.add(session);
+		log.info(session.getId() + "님이 입장하셨습니다.");
+		
+	}
 	
 	
     @Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		log.info("#ChattingHandler, handleMessage");
-		super.handleTextMessage(session, message);
-		
-		ObjectMapper objectMapper = new ObjectMapper();
-		Map<String, String> mapReceive = objectMapper.readValue(message.getPayload(), Map.class);
-		
-		switch (mapReceive.get("cmd")) {
-		case "CMD_ENTER":
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("bang_id", mapReceive.get("bang_id"));
-			map.put("session", session);
-			sessionList.add(map);
-			
-			for (int i = 0; i < sessionList.size(); i++) {
-				Map<String, Object> mapSessionList = sessionList.get(i);
-				String bang_id = (String) mapSessionList.get("bang_id");
-				WebSocketSession sess = (WebSocketSession) mapSessionList.get("session");
-				
-				if(bang_id.equals(mapReceive.get("bang_id"))) {
-					Map<String, String> mapToSend = new HashMap<String, String>();
-					mapToSend.put("bang_id", bang_id);
-					mapToSend.put("cmd", "CMD_ENTER");
-					mapToSend.put("msg", session.getId() +  "님이 입장 했습니다.");
-					
-					String jsonStr = objectMapper.writeValueAsString(mapToSend);
-					sess.sendMessage(new TextMessage(jsonStr));
-				}
+		System.out.println("받은 session 정보 : " + session.getAttributes());
+		System.out.println("입장 샷 : " + message);
+		for(WebSocketSession s : sessionList) {
+			if(session.getId() == s.getId()) {
+				continue;
 			}
-			break;
-
-		case "CMD_MSG_SEND":
-			for (int i = 0; i < sessionList.size(); i++) {
-				Map<String, Object> mapSessionList = sessionList.get(i);
-				String bang_id = (String) mapSessionList.get("bang_id");
-				WebSocketSession sess = (WebSocketSession) mapSessionList.get("session");
-
-				if (bang_id.equals(mapReceive.get("bang_id"))) {
-					Map<String, String> mapToSend = new HashMap<String, String>();
-					mapToSend.put("bang_id", bang_id);
-					mapToSend.put("cmd", "CMD_MSG_SEND");
-					mapToSend.put("msg", session.getId() + " : " + mapReceive.get("msg"));
-
-					String jsonStr = objectMapper.writeValueAsString(mapToSend);
-					sess.sendMessage(new TextMessage(jsonStr));
-				}
-			}
-			break;
+			s.sendMessage(new TextMessage(session.getId() + ":" + message.getPayload() + ":" + session.getAttributes()));
+			System.out.println("sessiong.getId(): " + session);
 		}
+		
+		System.out.println("sessionList : " + sessionList);
 		
 	}
     
@@ -132,42 +58,10 @@ public class ChattingHandler extends TextWebSocketHandler {
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		
 		log.info("#ChattingHandler, afterConnectionClosed");
+		sessionList.remove(session);
+		/*
 		
-		super.afterConnectionClosed(session, status);
-		
-		ObjectMapper objectMapper = new ObjectMapper();
-		String now_bang_id = "";
-		
-		for (int i = 0; i < sessionList.size(); i++) {
-			Map<String, Object> map = sessionList.get(i);
-			String bang_id = (String) map.get("bang_id");
-			WebSocketSession sess = (WebSocketSession) map.get("session");
-			
-			if(session.equals(sess)) {
-				now_bang_id = bang_id;
-				sessionList.remove(map);
-				break;
-			}	
-		}
-		
-		// 같은 채팅방에 퇴장 메세지 전송 
-		for (int i = 0; i < sessionList.size(); i++) {
-			Map<String, Object> mapSessionList = sessionList.get(i);
-			String bang_id = (String) mapSessionList.get("bang_id");
-			WebSocketSession sess = (WebSocketSession) mapSessionList.get("session");
-
-			if (bang_id.equals(now_bang_id)) {
-				Map<String, String> mapToSend = new HashMap<String, String>();
-				mapToSend.put("bang_id", bang_id);
-				mapToSend.put("cmd", "CMD_EXIT");
-				mapToSend.put("msg", session.getId() + "님이 퇴장 했습니다.");
-
-				String jsonStr = objectMapper.writeValueAsString(mapToSend);
-				sess.sendMessage(new TextMessage(jsonStr));
-			}
-		}
-		
-		
+		log.info(session.getPrincipal().getName() + "님이 퇴장하셨습니다.");
+		*/
 	}
-	
 }
